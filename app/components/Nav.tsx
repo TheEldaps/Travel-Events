2f255d4adc1ff3f6a07616c0593ce0cb111c1245
Eldaps <adejames97@gmail.com>
2025-11-10 15:54:54 +0100

    Fix nav bar to top of screen

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -14,7 +14,7 @@ export default function Nav() {
   }
 
   return (
-    <header className={`px-[5%] w-full bg-white pt-2.5 z-1 border-b ${navState ? 'block' : 'sticky top-0 '}`}>
+    <header className={`px-[5%] w-full bg-white pt-2.5 z-1 sticky top-0 border-b `}>
       <nav className="flex justify-between items-center  mb-2.5 ">
         <Image
           src={Logo}
