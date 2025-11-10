5aa727bdc07fc617b973bd8a854fad56c31a71b3
Eldaps <adejames97@gmail.com>
2025-11-10 04:12:54 +0100

    Add Service Section images

--- app/components/ServiceItems.tsx
+++ app/components/ServiceItems.tsx
@@ -1,9 +1,13 @@
 import Image, { StaticImageData } from 'next/image'
 
-export default function ServiceItems({ image, title, description }: { image: StaticImageData, titel: string, description: string }) {
+export default function ServiceItems({ image, title, description }: { image: StaticImageData, title: string, description: string }) {
     return (
         <div>
-            <Image src={image} />
+            <Image src={image} width='1000' height='400' alt='Service section image' />
+            <div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
         </div>
     )
 }
