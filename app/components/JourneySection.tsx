7aba508400d5222fcd34ddff9ebf96468a7f35a2
TheEldaps <adejames97@gmail.com>
2025-11-17 09:42:37 +0100

    Add Journey section Image cards

---
+++ app/components/JourneySection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function JourneySection() {
+  return (
+    <section className="px-[10%] bg-[#F2F7FF]">
+      <h2>
+        Our Journey Through <br />
+        <span className="text-[#8D8D8D]">images</span>
+      </h2>
+    </section>
+  );
+}
