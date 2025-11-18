1e54b77e89660e256c219ce27358143e9352b955
Eldaps <adejames97@gmail.com>
2025-11-18 12:09:57 +0100

    Style Request quote section form

--- app/components/FormItems.tsx
+++ app/components/FormItems.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function FormItems({ label, placeholder, }: { label: string, placeholder: string }) {
     return (
-        <div>
-            <label htmlFor={label}>{label}</label>
-            <input type="text" id={label} className='bg-[grey] text-' placeholder={placeholder} />
+        <div className='mb-3 grow text-[#2A2A2A]'>
+            <label htmlFor={label} className='text-[0.8rem] font-[dmsans] font-semibold'>{label}</label>
+
+            <input type="text" id={label} className='bg-[#F6F6F6] text-[0.8rem] px-2 py-1 rounded-md w-full ' placeholder={placeholder} />
         </div>
     )
 }
--- app/components/RequestQuoteSection.tsx
+++ app/components/RequestQuoteSection.tsx
@@ -1,58 +1,43 @@
-import React from 'react'
+import FormItems from "./FormItems"
+
 
 export default function RequestQuoteSection() {
     return (
-        <section>
-            <div className='text-center text-[1rem]'>
-                <h2 className=''>Request a <span>Quote. </span></h2>
-                <p>Please provide the following information </p>
-            </div>
-
-            <form action="" className='p-[5%]'>
-                <div className='flex'>
-                    <div>
-                        <label htmlFor="">Full Name</label>
-                        <input type="text" className='bg-[grey]' />
-                    </div>
-                    <div>
-                        <label htmlFor="">Phone Number</label>
-                        <input type="text" />
-                    </div>
-                </div>
-                <div>
-                    <label htmlFor="">Email Address</label>
-                    <input type="text" />
+        <section className='px-[5%] md:px-[10%] pt-10 border   '>
+            <section className='mx-auto md:bg-[url("/RequestQuoteBgImage.png")] bg-no-repeat bg-[position:center_bottom] bg-[length:80%_auto]
+ bg-contain'>
+                <div className=' text-center text-[1rem] mb-10'>
+                    <h2 className=''>Request a <span>Quote. </span></h2>
+                    <p>Please provide the following information </p>
                 </div>
 
-                <div>
-                    <label htmlFor="">Type of service needed</label>
-                    <input type="text" />
-                </div>
+                <form action="" className='p-[5%] mx-auto shadow-2xl bg-[white]  w-full md:w-[60%] rounded-xl'>
+                    <div className='sm:flex  gap-2'>
+                        <FormItems label='Full Name' placeholder='Your Full Name' />
 
-                <div>
-                    <label htmlFor="">Number of Guest</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <div>
-                        <label htmlFor="">Date</label>
-                        <input type="text" />
+                        <FormItems label='Phone Number' placeholder='Your Phone Number' />
                     </div>
 
-                    <div>
-                        <label htmlFor="">Time</label>
-                        <input type="text" />
+                    <FormItems label='Email Address' placeholder='Your Email Address' />
+
+                    <FormItems label='Enter the type of service required' placeholder='Your Full Name' />
+
+                    <FormItems label='Number of Guest' placeholder='Enter number of guests' />
+                    <div className='sm:flex gap-2 '>
+                        <FormItems label='Date' placeholder='Select Date' />
+
+                        <FormItems label='Time' placeholder='Select Time' />
                     </div>
-                </div>
-                <div>
-                    <label htmlFor="">Additional Information</label>
+                    <div className='mb-10'>
+                        <label htmlFor="" >Additional Information</label>
 
-                    <textarea name="" id=""></textarea>
-                </div>
-                <p>You may receive marketting and promotional materials. Contact the merchant for their privacy practices.</p>
+                        <textarea name="" id="" rows={5} placeholder='Type your message ' className='px-2 py-1 w-full bg-[#F6F6F6] text-[0.8rem]'></textarea>
+                    </div>
+                    <p className='text-[0.8rem] mb-2'>You may receive marketting and promotional materials. Contact the merchant for their privacy practices.</p>
 
-                <button>Submit </button>
-            </form>
+                    <button className=' block w-full text-white py-2 rounded-lg bg-black '>Submit </button>
+                </form>
+            </section>
 
         </section >
     )
