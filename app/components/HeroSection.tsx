cfa1fb2ca6b2d0df892e84b7ca9c52d58e390fb8
Eldaps <adejames97@gmail.com>
2025-11-22 17:09:30 +0100

    Fix hero text width

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import HeroImage from "../../public/HeroImage.png";
 export default function HeroSection() {
   return (
     <section id='home' className="pt-5 px-[5%] sm:px-[10%] mb-[50px]">
-      <h1 className="mb-4 lg:mb-10 w-[85%] mx-auto text-[2.5rem] leading px-[4%] sm:text-[3.9rem] md:text-[4.5rem] lg:text-[6rem] text-center font-semibold md:leading-22 lg:leading-28">
+      <h1 className="mb-4 lg:mb-10 mx-auto text-[2.5rem] leading px-[4%] sm:text-[3.9rem] md:text-[4.5rem] lg:text-[6rem] text-center font-semibold md:leading-22 lg:leading-28">
         Where Travel, Fun and Easy Planning Comes{" "}
         <br className="inline sm:hidden lg:inline" /> together
       </h1>
