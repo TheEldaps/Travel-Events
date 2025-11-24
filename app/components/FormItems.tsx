b917e0b2a668fa5b7d8266f8c1600537fdb86578
Eldaps <adejames97@gmail.com>
2025-11-24 04:03:55 +0100

    Remove input border on focus

--- app/components/FormItems.tsx
+++ app/components/FormItems.tsx
@@ -5,7 +5,7 @@ export default function FormItems({ label, placeholder, type = 'text' }: { label
         <div className='grow mb-5 flex flex-col text-[#2A2A2A]'>
             <label htmlFor={label} className='text-[0.8rem] font-dmsans font-semibold pb-1'>{label}</label>
 
-            <input type={type} id={label} className='bg-[#F6F6F6] text-[0.8rem] px-4 py-2 rounded-md w-full ' placeholder={placeholder} />
+            <input type={type} id={label} className='bg-[#F6F6F6] text-[0.8rem] px-4 py-2 rounded-md w-full focus:outline-0' placeholder={placeholder} />
         </div>
     )
 }
