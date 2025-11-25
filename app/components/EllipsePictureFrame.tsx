d4744b82319f6a56741eff77121690a61b47b626
Eldaps <adejames97@gmail.com>
2025-11-25 06:58:41 +0100

    mild px change under elipse picture frame

--- app/components/EllipsePictureFrame.tsx
+++ app/components/EllipsePictureFrame.tsx
@@ -6,7 +6,7 @@ import ElipseImage4 from '../../public/Ellipse4.png'
 
 export default function EllipsePictureFrame() {
     return (
-        <ul className='relative min-h-10 w-[20%] max-w-[400px] flex items-center justify-end '>
+        <ul className='relative min-h-10 w-[20%] mb-[10px] max-w-[400px] flex items-center justify-end '>
             <li className=' w-[30%] max-w-[50px] absolute right-[60%]'><Image src={ElipseImage1} width='50' height='50' alt='image for elipses frame' className='block h-auto' /></li>
             <li className=' w-[30%] max-w-[50px] absolute right-[40%]'><Image src={ElipseImage2} width='50' height='50' alt='image for elipses frame' className='block h-auto' /></li>
             <li className=' w-[30%] max-w-[50px] absolute right-[20%]'><Image src={ElipseImage3} width='50' height='50' alt='image for elipses frame' className='block h-auto' /></li >
